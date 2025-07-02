ad7e18e2b466d6608721ad697e192fa01ba24167
arty <itartyom01@gmail.com>
2025-07-02 19:22:55 +0700

    new aliases

--- bash/aliases.sh
+++ bash/aliases.sh
@@ -1,4 +1,14 @@
+alias salias='nvim ~/.config/bash/aliases.sh'
+
 alias edserv='sudo ranger /etc/systemd/system'
 alias ednginx='sudo ranger /etc/nginx'
+
 alias lg='lazygit'
+alias gp='git pull'
+
+alias dup='docker compose up'
+alias dupd='docker compose up -d'
+alias ddown='docker compose down'
+alias dlogs='docker compose logs'
+alias dlogsf='docker compose logs -f'
 
