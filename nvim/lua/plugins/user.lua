4a0f8aeae962c011ab9174764e19a32a347bc322
arty <itartyom01@gmail.com>
2025-07-15 10:30:07 +0700

    wrap fix

--- nvim/lua/plugins/user.lua
+++ nvim/lua/plugins/user.lua
@@ -1,21 +1,5 @@
 vim.g.neovide_scale_factor = 1.8
--- Установить нумерацию строк
 vim.opt.number = true -- Это эквивалентно 'set number'
-
--- Установить ширину текста
-vim.opt.textwidth = 0 -- Это эквивалентно 'set textwidth=0'
-
--- Установить отступ для переноса текста
-vim.opt.wrapmargin = 0 -- Это эквивалентно 'set wrapmargin=0'
-
--- Включить перенос строк
-vim.opt.wrap = true -- Это эквивалентно 'set wrap'
-
--- Перенос строк по словам, а не по символам
-vim.opt.linebreak = true -- Это эквивалентно 'set linebreak'
-
--- Установить количество колонок
--- Copy all text
 vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
@@ -29,6 +13,7 @@ return {
     keys = { -- Example mapping to toggle outline
       { "<F3>", "<cmd>Outline<CR>", desc = "Toggle outline" },
     },
+    opts = {},
   },
 
   { "lambdalisue/suda.vim" },
