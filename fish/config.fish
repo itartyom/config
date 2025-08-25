3901f9de64780d6b458d73d54b80a6dfc5a1b6c0
arty <itartyom01@gmail.com>
2025-08-25 19:15:15 +0700

    patch

--- fish/config.fish
+++ fish/config.fish
@@ -6,12 +6,8 @@ end
 set -x PATH $PATH \
     /usr/local/bin \
 
-alias ssh 'kitty +kitten ssh'
+alias kssh 'kitty +kitten ssh'
 alias fish-reload 'source ~/.config/fish/config.fish'
 
 set -g __autoenv_init_pwd "" && __autoenv
 
-
-# Added by OrbStack: command-line tools and integration
-# This won't be added again if you remove it.
-source ~/.orbstack/shell/init2.fish 2>/dev/null || :
